6bcff040c1701e349102242446cafa56949f332d
ShdowMoses <saandi536@gmail.com>
2024-10-25 15:29:38 +0700

    update

--- server/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import cors from "cors";
-import session from "express-session";
-
-const app = express();
-
-app.listen(process.env.APP_PORT, () => {
-  console.log("Server successfully running...");
-});
