19c4d6436c63d2cf237c70e966a0ef8a9fff8543
ShdowMoses <saandi536@gmail.com>
2024-10-31 14:58:26 +0700

    update

--- server/controllers/Product.js
+++ server/controllers/Product.js
@@ -1,25 +1,31 @@
 import Product from "../models/ProductModel.js";
 import User from "../models/UserModel.js";
+import { Op, where } from "sequelize";
 
+// get all products
 export const getProducts = async (req, res) => {
   try {
     let response;
     if (req.role === "admin") {
       response = await Product.findAll({
+        attributes: ["uuid", "name", "price"],
         include: [
           {
             model: User,
+            attributes: ["name", "email"],
           },
         ],
       });
     } else {
       response = await Product.findAll({
+        attributes: ["uuid", "name", "price"],
         where: {
           userId: req.userId,
         },
         include: [
           {
             model: User,
+            attributes: ["name", "email"],
           },
         ],
       });
@@ -29,7 +35,51 @@ export const getProducts = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
-export const getProductById = (req, res) => {};
+
+// get product by id
+export const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findOne({
+      where: {
+        uuid: req.params.id,
+      },
+    });
+    if (!product) return res.status(404).json({ message: "Product not found" });
+    let response;
+    if (req.role === "admin") {
+      response = await Product.findOne({
+        attributes: ["uuid", "name", "price"],
+        where: {
+          id: product.id,
+        },
+        include: [
+          {
+            model: User,
+            attributes: ["name", "email"],
+          },
+        ],
+      });
+    } else {
+      response = await Product.findOne({
+        attributes: ["uuid", "name", "price"],
+        where: {
+          [Op.and]: [{ id: product.id }, { userId: req.userId }],
+        },
+        include: [
+          {
+            model: User,
+            attributes: ["name", "email"],
+          },
+        ],
+      });
+    }
+    res.status(200).json({ response });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// create product
 export const createProducts = async (req, res) => {
   const { name, price } = req.body;
   try {
@@ -43,5 +93,65 @@ export const createProducts = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
-export const updateProducts = (req, res) => {};
-export const deleteProducts = (req, res) => {};
+
+// update product
+export const updateProducts = async (req, res) => {
+  try {
+    const product = await Product.findOne({
+      where: {
+        uuid: req.params.id,
+      },
+    });
+    if (!product) return res.status(404).json({ message: "Product not found" });
+    const { name, price } = req.body;
+    const updateData = { name, price };
+    if (req.role === "admin") {
+      await Product.update(updateData, {
+        where: {
+          id: product.id,
+        },
+      });
+    } else {
+      if (req.userId !== product.userId)
+        return res.status(403).json({ message: "Access denied" });
+      await Product.update(updateData, {
+        where: {
+          [Op.and]: [{ id: product.id }, { userId: req.userId }],
+        },
+      });
+    }
+    res.status(200).json({ message: "Success update product" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// update product
+export const deleteProducts = async (req, res) => {
+  try {
+    const product = await Product.findOne({
+      where: {
+        uuid: req.params.id,
+      },
+    });
+    if (!product) return res.status(404).json({ message: "Product not found" });
+    if (req.role === "admin") {
+      await Product.destroy({
+        where: {
+          id: product.id,
+        },
+      });
+    } else {
+      if (req.userId !== product.userId)
+        return res.status(403).json({ message: "Access denied" });
+      await Product.destroy({
+        where: {
+          [Op.and]: [{ id: product.id }, { userId: req.userId }],
+        },
+      });
+    }
+    res.status(200).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
